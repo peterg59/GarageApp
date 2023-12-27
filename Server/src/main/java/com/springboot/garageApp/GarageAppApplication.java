391c6c5f2d6f698f2d95d1df6430e3afba7a44df
Pierre Guyard <pguyard59@gmail.com>
2023-12-27 23:50:24 +0100

    changement bdd Derby vers H2

--- Server/src/main/java/com/springboot/garageApp/GarageAppApplication.java
+++ Server/src/main/java/com/springboot/garageApp/GarageAppApplication.java
@@ -2,14 +2,8 @@ package com.springboot.garageApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.springboot.garageApp.*")
-@EntityScan(basePackages = "com.springboot.garageApp.*")
-@EnableJpaRepositories(basePackages = "com.springboot.garageApp.*")
 public class GarageAppApplication {
 
 	public static void main(String[] args) {

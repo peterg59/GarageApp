cbf6dd7482588853ddefbbe49205bc13ec5f575c
Pierre GUYARD <pierre.guyard@sii.fr>
2025-04-14 16:45:32 +0200

    Modification mineure des entités

--- Server/src/main/java/com/example/garageApp/model/Car.kt
+++ Server/src/main/java/com/example/garageApp/model/Car.kt
@@ -3,35 +3,20 @@ package com.example.garageApp.model
 import jakarta.persistence.*
 
 @Entity
-class Car {
+data class Car(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long,
+    var model: String? = null,
+    var brand: String? = null,
+    @Column(name = "released_year")
+    var year: Int = 0,
+    var color: Color? = null
+) {
     enum class Color {
         RED,
         BLUE,
         GREEN,
         YELLOW
-    }
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    var id: Long = 0
-    @JvmField
-    var model: String? = null
-    @JvmField
-    var brand: String? = null
-
-    @JvmField
-    @Column(name = "released_year")
-    var year: Int = 0
-    @JvmField
-    var color: Color? = null
-
-    constructor()
-
-    constructor(id: Long, model: String?, brand: String?, year: Int, color: Color?) {
-        this.id = id
-        this.model = model
-        this.brand = brand
-        this.year = year
-        this.color = color
     }
 }
--- Server/src/main/java/com/example/garageApp/model/Customer.kt
+++ Server/src/main/java/com/example/garageApp/model/Customer.kt
@@ -1,34 +1,19 @@
 package com.example.garageApp.model
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
 @Entity
-class Customer {
+data class Customer(
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    var id: Long = 0
-    var lastName: String? = null
-    var firstName: String? = null
-    var mail: String? = null
-    var phoneNumber: Int = 0
-    var address: String? = null
+    val id: Long,
+    @Column(name = "last_name")
+    var lastName: String? = null,
+    @Column(name = "first_name")
+    var firstName: String? = null,
+    var mail: String? = null,
+    @Column(name = "phone_number")
+    var phoneNumber: Int = 0,
+    var address: String? = null,
     var age: Int = 0
-
-    constructor()
-
-    constructor(
-        id: Long, lastName: String?, firstName: String?, mail: String?, phoneNumber: Int,
-        address: String?, age: Int
-    ) {
-        this.id = id
-        this.lastName = lastName
-        this.firstName = firstName
-        this.mail = mail
-        this.phoneNumber = phoneNumber
-        this.address = address
-        this.age = age
-    }
-}
+)
